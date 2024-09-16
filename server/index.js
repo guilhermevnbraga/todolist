efeceb16d63e6775ef1b9054ffc4edd8a6c7ed8d
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2024-09-16 10:26:59 -0300

    refactor: Update server index.js to add API status route and use arrow functions

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import tarefaRouter from './routes/tarefa.routes.js';
-import membroRouter from './routes/membro.routes.js';
+import express from "express";
+import cors from "cors";
+import tarefaRouter from "./routes/tarefa.routes.js";
+import membroRouter from "./routes/membro.routes.js";
 
 const app = express();
 
 const corsOptions = {
-  origin: process.env.ORIGIN,
-  optionsSuccessStatus: 200
+    origin: process.env.ORIGIN,
+    optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use('/tarefa', tarefaRouter);
-app.use('/membro', membroRouter);
-app.route('/').get((req, res) => {
-  res.send('API em execução');
-}
+app.use("/tarefa", tarefaRouter);
+app.use("/membro", membroRouter);
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
