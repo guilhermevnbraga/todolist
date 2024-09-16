fef9dd2e33c29ddc6ad258b919788004e0d4ae3b
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2024-09-16 10:51:03 -0300

    fix: Update CORS options in server/index.js

--- server/index.js
+++ server/index.js
@@ -6,11 +6,20 @@ import membroRouter from "./routes/membro.routes.js";
 const app = express();
 
 const corsOptions = {
-    origin: process.env.ORIGIN,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
-    credentials: true,
-    optionsSuccessStatus: 204,
+  origin: (origin, callback) => {
+      if (!origin || process.env.ORIGIN === origin) {
+          callback(null, true);
+      } else {
+          callback(new Error("Not allowed by CORS"));
+      }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(cors(corsOptions));
 app.use(express.json());
