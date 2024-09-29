8a1de3241ab2d77a8e70438594f9dabac1c8b5e7
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2024-09-28 23:25:43 -0300

    fix: cors

--- server/index.js
+++ server/index.js
@@ -5,23 +5,25 @@ import membroRouter from "./routes/membro.routes.js";
 
 const app = express();
 
+const allowedOrigins = [
+    "https://todolist-nine-gules.vercel.app",
+    "http://localhost:3000",
+];
 const corsOptions = {
-    origin: process.env.ORIGIN,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization;"],
-    optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://todolist-nine-gules.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.sendStatus(200); // Responder com status 200 OK
-  });
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use("/tarefa", tarefaRouter);
