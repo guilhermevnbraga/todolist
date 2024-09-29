6e5f0da1c3be9f04e934d540a93e26b89b3d7e27
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2024-09-28 23:30:29 -0300

    fix: cors

--- server/index.js
+++ server/index.js
@@ -5,26 +5,15 @@ import membroRouter from "./routes/membro.routes.js";
 
 const app = express();
 
-const allowedOrigins = [
-    "https://todolist-nine-gules.vercel.app",
-    "http://localhost:3000",
-];
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization;"],
+    optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-
 app.use(express.json());
 app.use("/tarefa", tarefaRouter);
 app.use("/membro", membroRouter);
